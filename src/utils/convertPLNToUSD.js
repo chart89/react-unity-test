4f1bf5ba8aadd96f172b856bfffc5cee7a961d96
Darek Biernat <kaiju83@outlook.com>
2023-07-27 20:11:57 +0200

    Add descriptions to function convertPLNToUSD.js

--- src/utils/convertPLNToUSD.js
+++ src/utils/convertPLNToUSD.js
@@ -7,19 +7,22 @@ export const convertPLNToUSD = (PLN) => {
     currency: 'USD'
   });
 
-
+  /* if imput is 'string' return NaN */
   if(typeof PLN === 'string') {
     return NaN;
   };
 
+  /* if imput is empty retur NaN */
   if (PLN == null) {
     return NaN;
   }
 
+  /* if imput is not a number and is not string return Error */
   if(typeof PLN !== 'string' && typeof PLN !== 'number') {
     return 'Error';
   };
 
+  /* if imput is lower than o return $0.00 */
   if(PLN < 0) {
     return ('$0.00');
   }
