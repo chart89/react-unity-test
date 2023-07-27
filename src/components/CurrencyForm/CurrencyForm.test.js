7cfa287e6e6ab6ba7df090037326621c57dcbda7
Darek Biernat <kaiju83@outlook.com>
2023-07-27 22:39:16 +0200

    Upgrade test CurrencyForm - add more functionalities

--- src/components/CurrencyForm/CurrencyForm.test.js
+++ src/components/CurrencyForm/CurrencyForm.test.js
@@ -1,12 +1,21 @@
 import CurrencyForm from './CurrencyForm';
 import userEvent from '@testing-library/user-event';
-import { render, screen } from '@testing-library/react';
+import { render, screen, cleanup } from '@testing-library/react';
+
+const testCases = [
+  { amount: '100', from: 'PLN', to: 'USD' },
+  { amount: '20', from: 'USD', to: 'PLN' },
+  { amount: '200', from: 'PLN', to: 'USD' },
+  { amount: '345', from: 'USD', to: 'PLN' },
+];
 
 describe('Component CurrencyForm', () => {
   it('should render without crashing', () => {
     render(<CurrencyForm action={() => {}} />);
   });
   it('should run action callback with proper data on form submit', () => {
+
+    for(const testObj of testCases) {
     const action = jest.fn();
   
     // render component
@@ -19,17 +28,19 @@ describe('Component CurrencyForm', () => {
     const amountField = screen.getByTestId('amount');
     const fromField = screen.getByTestId('from-select');
     const toField = screen.getByTestId('to-select');
-  
+
     // set test values to fields
-    userEvent.type(amountField, '100');
-    userEvent.selectOptions(fromField, 'PLN');
-    userEvent.selectOptions(toField, 'USD');
+    userEvent.type(amountField, testObj.amount);
+    userEvent.selectOptions(fromField, testObj.from);
+    userEvent.selectOptions(toField, testObj.to);
   
     // simulate user click on "convert" button
     userEvent.click(submitButton);
   
     // check if action callback was called once and with proper argument
     expect(action).toHaveBeenCalledTimes(1);
-    expect(action).toHaveBeenCalledWith({ amount: 100, from: 'PLN', to: 'USD' });
+    expect(action).toHaveBeenCalledWith({ amount: parseInt(testObj.amount), from: testObj.from, to: testObj.to });
+    cleanup()
+    }
   });
 });
